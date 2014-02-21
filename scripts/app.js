ea34aa18fee0cda99457bf15b6900fc0e9924ed0
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-02-21 10:21:30 -0600

    Add navigation.

--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,29 @@
 
     app = angular.module("app", ["ngRoute"]);
 
+    app.config(function ($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider.when("/continuous", {
+            templateUrl: "partials/continuous-outcome.html",
+            controller: "ContinuousOutcomeController"
+        }).when("/binary", {
+            templateUrl: "partials/binary-outcome.html",
+            controller: "BinaryOutcomeController"
+        }).when("/survival", {
+            templateUrl: "partials/survival-outcome.html",
+            controller: "SurvivalOutcomeController"
+        }).otherwise({
+            redirectTo: "/continuous"
+        });
+    });
+
+    app.controller("ContinuousOutcomeController", function ($scope) {
+    });
+
+    app.controller("BinaryOutcomeController", function ($scope) {
+    });
+
+    app.controller("SurvivalOutcomeController", function ($scope) {
+    });
+
 }());
